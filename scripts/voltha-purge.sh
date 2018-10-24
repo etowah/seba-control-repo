bd54cfd3bbda4d76feb1e914eae0932e20920533
Matt Jeanneret <mj3580@att.com>
2018-10-24 06:44:38 -0400

    Fix bashrc reference.  Remove debug from yaml.  Add script

---
+++ scripts/voltha-purge.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+clusterip=$(dig +short etcd-cluster-client.default.svc.cluster.local @10.233.0.3)
+
+kubectl scale statefulset vcore -n voltha --replicas=0
+kubectl scale deployment ofagent -n voltha --replicas=0
+kubectl scale deployment onos --replicas=0
+
+etcdctl --endpoints=http://${clusterip}:2379 del service --prefix -w fields
+sleep 3
+
+kubectl scale statefulset vcore -n voltha --replicas=1
+kubectl scale deployment ofagent -n voltha --replicas=1
+kubectl scale deployment onos --replicas=1
+
