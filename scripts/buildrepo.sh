fceafe4f75510cba635773999997acf80d5df4a1
Matt Jeanneret <mj3580@att.com>
2018-10-25 10:52:04 -0400

    Big changes needed for offline kubespray installation.  Also integrated apt mirror setup

--- scripts/buildrepo.sh
+++ scripts/buildrepo.sh
@@ -1,6 +1,32 @@
+#!/bin/bash
+
 #This can be used to build and apt repo from a set of files. 
 #The files used to build the current version are stored in the apt-mirror-lists folder
-#Copy this and the lists files into the same directory, then run this to generate a mirror
 
-ls install-*.list | xargs cat | xargs apt download 
-dpkg-scanpackages -m . /dev/null | gzip -9c >  Packages.gz
+listdir=$1
+packagedir=$2
+currentdir=$(pwd)
+
+if [ -z "$listdir" ] || [ -z "$packagedir" ]
+then
+  echo "$0 <directory-apt-mirror-lists> <pkg-output-dir>"
+  exit 1
+fi
+
+for i in $(ls -1 $listdir/install-*.list)
+do
+  echo "List: $i"
+
+  pkglist=$(cat $i)
+
+  cd $packagedir
+  for j in $pkglist
+  do
+    echo "Package: $j"
+    apt download $j
+  done
+  cd $currentdir
+done
+
+set -x
+dpkg-scanpackages -m $packagedir /dev/null | gzip -9c >  $packagedir/Packages.gz
