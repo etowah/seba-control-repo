4c89cbcda4f556bd7388b88aae6c4b7365b2925c
Matt Jeanneret <mj3580@att.com>
2018-11-30 16:18:46 -0500

    Fixed onos app reload instructions.  add a bit more time on voltha purge

--- scripts/voltha-purge.sh
+++ scripts/voltha-purge.sh
@@ -6,11 +6,13 @@ clusterip=$(dig +short etcd-cluster-client.default.svc.cluster.local @10.233.0.3
 kubectl scale statefulset vcore -n voltha --replicas=0
 kubectl scale deployment ofagent -n voltha --replicas=0
 kubectl scale deployment onos --replicas=0
+kubectl scale deployment voltha -n voltha --replicas=0
 
 etcdctl --endpoints=http://${clusterip}:2379 del service --prefix -w fields
-sleep 3
+sleep 20
 
 kubectl scale statefulset vcore -n voltha --replicas=1
 kubectl scale deployment ofagent -n voltha --replicas=1
 kubectl scale deployment onos --replicas=1
+kubectl scale deployment voltha -n voltha --replicas=1
 
